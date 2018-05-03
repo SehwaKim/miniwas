9b26df4dba7cafa3d11f2a8a8e74c9491e6bc3cd
김세화 <sehwakm@gmail.com>
2018-05-03 21:35:09 +0900

    요청path와 서블릿이 매핑된 RequestMapping 객체 생성

---
+++ src/main/java/examples/RequestMapping.java
@@ -0,0 +1,33 @@
+package examples;
+
+import examples.servlet.Servlet;
+
+public class RequestMapping {
+    private String path;
+    private String servletClassName;
+    private Servlet servlet;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getServletClassName() {
+        return servletClassName;
+    }
+
+    public void setServletClassName(String servletClassName) {
+        this.servletClassName = servletClassName;
+    }
+
+    public Servlet getServlet() {
+        return servlet;
+    }
+
+    public void setServlet(Servlet servlet) {
+        this.servlet = servlet;
+    }
+}
