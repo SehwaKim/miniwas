744fc2c9404fc45c2747c325059dfdbfa5fe4a5d
김세화 <sehwakm@gmail.com>
2018-05-03 21:28:29 +0900

    Initial commit

---
+++ src/main/java/examples/WASMain.java
@@ -0,0 +1,8 @@
+package examples;
+
+public class WASMain {
+    public static void main(String[] args) {
+        WebApplicationServer was = new WebApplicationServer(8080);
+        new Thread(was).start();
+    }
+}
